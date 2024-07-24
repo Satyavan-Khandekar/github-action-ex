114407f35f48f880a7446f94f84c8c16357f6f07
Satyavan-Khandekar <satyavankhandekar3@gmail.com>
2024-07-24 19:06:43 +0530

    added docker configuration

--- src/main/java/com/example/demo/GithubActionsCicdApplication.java
+++ src/main/java/com/example/demo/GithubActionsCicdApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class GithubActionsCicdApplication {
 
+	@GetMapping("/display")
 	public String welcome()
 	{
 		return "This is demo for cicd pipeline using github actions";
